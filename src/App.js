fcc6a0c44ce30872b8462e436aff0e241ff9c6f9
karammad10 <karam.madrid.7@gmail.com>
2019-04-10 00:25:05 +0200

    selectbox interest fix

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ class App extends Component {
     selectedFormValue: "",
     selectedAmount: 5000,
     selectedDuration: 0,
-    durationOption: 3,
-    interest: 6
+    durationOption: 3
   };
 
-  computeInterestRate(duration, amount) {
+  computeInterestRate = (duration, amount) => {
     if (duration <= 19) {
       if (amount <= 50000) return 6;
       if (amount <= 150000) return 5;
@@ -26,9 +25,9 @@ class App extends Component {
     if (amount <= 50000) return 8;
     if (amount <= 150000) return 7;
     return 6;
-  }
+  };
 
-  handleSelectChange = e => {
+  handleFormSelect = e => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
   };
@@ -50,16 +49,10 @@ class App extends Component {
       });
 
       this.setState({
-        [name]: parseInt(value, 10),
+        selectedDuration: parseInt(value, 10),
         durationOption: closestMonth
       });
     }
-
-    const { selectedAmount, selectedDuration } = this.state;
-
-    this.setState({
-      interest: this.computeInterestRate(selectedDuration, selectedAmount)
-    });
   };
 
   render() {
@@ -68,127 +61,125 @@ class App extends Component {
       selectedFormValue,
       selectedDuration,
       durationOption,
-      selectedAmount,
-      interest
+      selectedAmount
     } = this.state;
+    const interest = this.computeInterestRate(selectedDuration, selectedAmount);
     return (
-      <div className="container">
-        <div className="App">
-          <h3 className="header">Put together your business financing</h3>
-          <div>
-            <form>
-              <div className="requested-value-container">
-                <label className="mini-title">Target</label>
-                <label id="m-title" className="mini-title">
-                  Company form
-                </label>
-              </div>
-              <div className="requested-value-container">
-                <select
-                  name="selectedTargetValue"
-                  className="select-value"
-                  value={selectedTargetValue}
-                  onChange={this.handleSelectChange}
-                >
-                  <option value="" disabled>
-                    Choose target
-                  </option>
-                  <option value="marketing">marketing</option>
-                  <option value="equipment">equipment</option>
-                </select>
+      <div className="App">
+        <h3 className="header">Put together your business financing</h3>
+        <div>
+          <form>
+            <div className="div-container">
+              <label className="mini-title">Target</label>
+              <label id="m-title" className="mini-title">
+                Company form
+              </label>
+            </div>
+            <div className="div-container">
+              <select
+                name="selectedTargetValue"
+                className="select-value"
+                value={selectedTargetValue}
+                onChange={this.handleFormSelect}
+              >
+                <option value="" disabled>
+                  Choose target
+                </option>
+                <option value="marketing">marketing</option>
+                <option value="equipment">equipment</option>
+              </select>
 
-                <select
-                  id="nd-select"
-                  name="selectedFormValue"
-                  className="select-value"
-                  value={selectedFormValue}
-                  onChange={this.handleSelectChange}
-                >
-                  <option value="" disabled>
-                    Choose form
-                  </option>
-                  <option value="bv">bv</option>
-                  <option value="eenmanszaak">eenmanszaak</option>
-                </select>
-              </div>
+              <select
+                id="nd-select"
+                name="selectedFormValue"
+                className="select-value"
+                value={selectedFormValue}
+                onChange={this.handleFormSelect}
+              >
+                <option value="" disabled>
+                  Choose form
+                </option>
+                <option value="bv">bv</option>
+                <option value="eenmanszaak">eenmanszaak</option>
+              </select>
+            </div>
 
-              <div className="requested-value-container">
-                <label className="mini-title">Financing</label>
-                <span className="input-euro left">
-                  <input
-                    name="selectedAmount"
-                    type="text"
-                    value={(selectedAmount / 1000).toFixed(3)}
-                    id="amount-input"
-                    onChange={this.handleInterestChange}
-                  />
-                </span>
-              </div>
-
-              <div className="range">
+            <div className="div-container">
+              <label className="mini-title">Financing</label>
+              <span className="input-euro left">
                 <input
-                  className="range-slide"
                   name="selectedAmount"
-                  type="range"
-                  min="5.000"
-                  max={
-                    selectedTargetValue === "equipment" &&
-                    selectedFormValue === "bv"
-                      ? "500.000"
-                      : "250.000"
-                  }
-                  step="1.00"
+                  type="text"
                   value={(selectedAmount / 1000).toFixed(3)}
+                  id="amount-input"
                   onChange={this.handleInterestChange}
                 />
-              </div>
+              </span>
+            </div>
 
-              <div className="interest-container">
-                <span id="interest-title">Interest:</span>
-                <div id="interest">{interest}%</div>
-              </div>
+            <div className="range">
+              <input
+                className="range-slide"
+                name="selectedAmount"
+                type="range"
+                min="5.000"
+                max={
+                  selectedTargetValue === "equipment" &&
+                  selectedFormValue === "bv"
+                    ? "500.000"
+                    : "250.000"
+                }
+                step="1.00"
+                value={(selectedAmount / 1000).toFixed(3)}
+                onChange={this.handleInterestChange}
+              />
+            </div>
 
-              <div className="requested-value-container">
-                <label className="mini-title">Duration</label>
-                <select
-                  id="d-select"
-                  name="selectedDuration"
-                  value={durationOption}
-                  className="select-value"
-                  onChange={this.handleInterestChange}
-                >
-                  <option disabled>Choose duration</option>
-                  {monthArray.map((month, index) => (
-                    <option key={index} value={month}>
-                      {month} months
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div className="range">
-                <input
-                  className="range-slide"
-                  name="selectedDuration"
-                  type="range"
-                  min={1}
-                  max={60}
-                  step="1"
-                  value={selectedDuration}
-                  onChange={this.handleInterestChange}
-                />
-              </div>
-              <div id="footer">
-                <a href=".">Check if you qualify</a>
-                <button className="submit-button">
-                  <span id="button-text">Get started right away</span>
-                </button>
-              </div>
-            </form>
-            <p id="contact">
-              Calculate an automatically generated customized offer. Please
-              contact 020-2621800 for questions or comments..
-            </p>
-          </div>
+            <div className="interest-container">
+              <span id="interest-title">Interest:</span>
+              <div id="interest">{interest}%</div>
+            </div>
+
+            <div className="div-container">
+              <label className="mini-title">Duration</label>
+              <select
+                id="d-select"
+                name="selectedDuration"
+                value={durationOption}
+                className="select-value"
+                onChange={this.handleInterestChange}
+              >
+                <option disabled>Choose duration</option>
+                {monthArray.map((month, index) => (
+                  <option key={index} value={month}>
+                    {month} months
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="range">
+              <input
+                className="range-slide"
+                name="selectedDuration"
+                type="range"
+                min={1}
+                max={60}
+                step="1"
+                value={selectedDuration}
+                onChange={this.handleInterestChange}
+              />
+            </div>
+            <div id="footer">
+              <a href=".">Check if you qualify</a>
+              <button className="submit-button">
+                <span id="button-text">Get started right away</span>
+              </button>
+            </div>
+          </form>
+          <p id="contact">
+            Calculate an automatically generated customized offer. Please
+            contact 020-2621800 for questions or comments..
+          </p>
         </div>
       </div>
     );
